64aa1d840c8dc79e1b035a29d05758e4ed42553e
priteshbharakhadaildc4 <pritesh.bharakhada.ildc@gmail.com>
2014-10-10 08:59:42 +0530

    New Project Template added

---
+++ Remind_Me/Remind_Me/OtherClasses/RMViewController.h
@@ -0,0 +1,13 @@
+//
+//  RMViewController.h
+//  Remind_Me
+//
+//  Created by indianic on 10/10/14.
+//  Copyright (c) 2014 ILDC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RMViewController : UIViewController
+
+@end
