c7aba9dc6fa6a6c1280ad2310504421ddd09e3b5
pritesh bharakhada <pritesh.bharakhada.ildc@gmail.com>
2014-10-10 08:22:04 +0530

    New Project Templet Added

---
+++ MyAppTemp/MyAppTemp/Classes/SplashViewController/MPSplashViewController.h
@@ -0,0 +1,13 @@
+//
+//  MPSplashViewController.h
+//  MyAppTemp
+//
+//  Created by indianic on 10/10/14.
+//  Copyright (c) 2014 ILDC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MPSplashViewController : UIViewController
+
+@end
---
+++ MyAppTemp/MyAppTemp/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MyAppTemp
+//
+//  Created by indianic on 10/10/14.
+//  Copyright (c) 2014 ILDC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MPAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MPAppDelegate class]));
+    }
+}
